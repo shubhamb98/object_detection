0cd1e40f6fa0cdcd4eeced8f70d5abfc1e2df597
Shubham <shubham.bhavsar@gmail.com>
2019-04-01 14:44:49 +0530

    Initial commit

---
+++ aa.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request,urlopen
+import re
+import numpy as np
+from bb import scores,classes,category_index,image_np
+from PIL import Image
+def quote(topic):
+    req = Request("https://www.brainyquote.com/topics/"+topic, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage)
+    x=[]
+    for link in soup.findAll('a', attrs={'title':'view quote' }):
+        x.append(link)
+    cleantext=[]
+    for i in x:
+        cleantext.append(BeautifulSoup(str(i), "html.parser").text)
+    return cleantext
+
+
+def object_detect_lables():
+    final_score = np.squeeze(scores)
+    count = 0
+    for i in range(100):
+        if scores is None or final_score[i] > 0.5:
+                count = count + 1
+    print('count',count)
+    printcount =0
+    for i in classes[0]:
+          printcount = printcount +1
+          x=category_index[i]['name']
+
+          if(printcount == count):
+                break
+    return x
+
+
+xx=quote(object_detect_lables())
+
+for i in xx:
+    print(i)
+
+im=Image.fromarray(image_np)
+im.show()
